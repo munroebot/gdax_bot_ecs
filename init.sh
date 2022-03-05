f692df4883287d3a793be368c86750679bd8b668
Brian Munroe <brian.e.munroe@gmail.com>
2022-03-05 12:36:55 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,11 +3,11 @@
 # init.sh
 
 # Fetch bot configuration from parameter store
-x=$(aws ssm get-parameter --with-decryption --name gdax_bot_settings_conf);
+x=$(aws ssm get-parameter --region us-west-2 --with-decryption --name gdax_bot_settings_conf);
 echo -e $x > /opt/gdax_bot/settings.conf
 
 # Fetch task runner script from paramter store
-j=$(aws ssm get-parameter --name gdaxbot_trades_$INTERVAL.sh);
+j=$(aws ssm get-parameter  --region us-west-2 --name gdaxbot_trades_$INTERVAL.sh);
 echo -e $j > /opt/gdax_bot/trades_${INTERVAL}.sh;
 chmod +x /opt/gdax_bot/trades_${INTERVAL}.sh;
 
